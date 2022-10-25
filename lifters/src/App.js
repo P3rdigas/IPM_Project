f035644ff9c80597ddedc7a07881dbfe957d328f
P3rdigas <pedro.perdigao2001@gmail.com>
2022-10-25 03:41:17 +0100

    Home Page Navbar

--- lifters/src/App.js
+++ lifters/src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import IntroNavbar from "./Components/IntroNavbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <IntroNavbar />
+        <Routes>
+          <Route path="/" exact/>
+        </Routes>
+      </Router>
+    </>
   );
 }
 
