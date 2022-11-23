a323c3e25934cc8c8046debb55425904daa3902c
Pedro <pmv.lourenco@campus.fct.unl.pt>
2022-11-23 13:55:44 +0000

    cards e cenas

--- lifters/src/pages/Overview/components/Card/cardGym.js
+++ lifters/src/pages/Overview/components/Card/cardGym.js
@@ -4,20 +4,23 @@ import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import { GrCircleInformation } from "react-icons/gr";
 
-function CardGym({title, body}) {
+function CardGym({title, body, body2, body3}) {
   return (
-    <Card sx={{ maxWidth: 345 }}>
+    <Card sx={{ minWidth: 250, maxWidth:250, minHeight: 230, maxHeight:230  }}>
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
           {title}
         </Typography>
         <Typography variant="body2" color="text.secondary">
-          {body}
+          <h2>{body}</h2>
+          <h2>{body2}</h2>
+          <h2>{body3}</h2>
         </Typography>
       </CardContent>
       <CardActions>
-        <Button size="small">Learn More</Button>
+        <GrCircleInformation className='workout-card-info'/>
       </CardActions>
     </Card>
   );
--- lifters/src/pages/Overview/components/SavedWorkouts/savedWorkouts.js
+++ lifters/src/pages/Overview/components/SavedWorkouts/savedWorkouts.js
@@ -1,67 +1,76 @@
 import * as React from 'react';
 import CardGym from "../Card/cardGym";
 import './savedWorkouts.css'
+
 function SavedWorkouts() {
     return (
         <div className='saved-wo'>
             <div class='saved-wo-title'>
-            <h2>Your Saved Workouts</h2>
+                <h2>Your Saved Workouts</h2>
             </div>
             <div className='all-cards'>
                 <div className='card1'>
                     <CardGym className='card1-c'
-                    title='o'
-                    body='Vítor Campelos, treinador do Chaves, considera que o empate no terreno do Vizela (2-2), na entrada em cena na fase de grupos da Taça da Liga, foi inteiramente justo.
-                    Vítor Campelos, treinador do Chaves, considera que o empate no terreno do Vizela (2-2), na entrada em cena na fase de grupos da Taça da Liga, foi inteiramente justo'
+                    title='Arms Training'
+                    body='Bicep'
+                    body2='Tricep'
+                    body3='Vareta'
                     />                
                 </div>
                 <div className='card2'>
                     <CardGym className='card2-c'
-                    title='benny'
-                    body='Vítor Campelos, treinador do Chaves, considera que o empate no terreno do Vizela (2-2), na entrada em cena na fase de grupos da Taça da Liga, foi inteiramente justo.
-                    Vítor Campelos, treinador do Chaves, considera que o empate no terreno do Vizela (2-2), na entrada em cena na fase de grupos da Taça da Liga, foi inteiramente justo'
+                    title='Arms Training'
+                    body='Bicep'
+                    body2='Tricep'
+                    body3='Vareta'
                     />
                 </div>
                 <div className='card3'>
                     <CardGym className='card3-c'
-                    title='e'
-                    body='Vítor Campelos, treinador do Chaves, considera que o empate no terreno do Vizela (2-2), na entrada em cena na fase de grupos da Taça da Liga, foi inteiramente justo.
-                    Vítor Campelos, treinador do Chaves, considera que o empate no terreno do Vizela (2-2), na entrada em cena na fase de grupos da Taça da Liga, foi inteiramente justo'
+                    title='Arms Training'
+                    body='Bicep'
+                    body2='Tricep'
+                    body3='Vareta'
                     />                
                 </div>
                 <div className='card4'>
                     <CardGym className='card4-c'
-                    title='o'
-                    body='Vítor Campelos, treinador do Chaves, considera que o empate no terreno do Vizela (2-2), na entrada em cena na fase de grupos da Taça da Liga, foi inteiramente justo.
-                    Vítor Campelos, treinador do Chaves, considera que o empate no terreno do Vizela (2-2), na entrada em cena na fase de grupos da Taça da Liga, foi inteiramente justo'
+                    title='Arms Training'
+                    body='Bicep'
+                    body2='Tricep'
+                    body3='Vareta'
                     />
                 </div>
                 <div className='card5'>
                     <CardGym className='card5-c'
-                    title='perdigao'
-                    body='Vítor Campelos, treinador do Chaves, considera que o empate no terreno do Vizela (2-2), na entrada em cena na fase de grupos da Taça da Liga, foi inteiramente justo.
-                    Vítor Campelos, treinador do Chaves, considera que o empate no terreno do Vizela (2-2), na entrada em cena na fase de grupos da Taça da Liga, foi inteiramente justo'
+                    title='Arms Training'
+                    body='Bicep'
+                    body2='Tricep'
+                    body3='Vareta'
                     />                
                 </div>
                 <div className='card6'>
                     <CardGym className='card6-c'
-                    title='curtem'
-                    body='Vítor Campelos, treinador do Chaves, considera que o empate no terreno do Vizela (2-2), na entrada em cena na fase de grupos da Taça da Liga, foi inteiramente justo.
-                    Vítor Campelos, treinador do Chaves, considera que o empate no terreno do Vizela (2-2), na entrada em cena na fase de grupos da Taça da Liga, foi inteiramente justo'
+                    title='Arms Training'
+                    body='Bicep'
+                    body2='Tricep'
+                    body3='Vareta'
                     />
                 </div>
                 <div className='card7'>
                     <CardGym className='card7-c'
-                    title='de'
-                    body='Vítor Campelos, treinador do Chaves, considera que o empate no terreno do Vizela (2-2), na entrada em cena na fase de grupos da Taça da Liga, foi inteiramente justo.
-                    Vítor Campelos, treinador do Chaves, considera que o empate no terreno do Vizela (2-2), na entrada em cena na fase de grupos da Taça da Liga, foi inteiramente justo'
+                    title='Arms Training'
+                    body='Bicep'
+                    body2='Tricep'
+                    body3='Vareta'
                     />                
                 </div>
                 <div className='card8'>
                     <CardGym className='card8-c'
-                    title='vareta'
-                    body='Vítor Campelos, treinador do Chaves, considera que o empate no terreno do Vizela (2-2), na entrada em cena na fase de grupos da Taça da Liga, foi inteiramente justo.
-                    Vítor Campelos, treinador do Chaves, considera que o empate no terreno do Vizela (2-2), na entrada em cena na fase de grupos da Taça da Liga, foi inteiramente justo'
+                    title='Arms Training'
+                    body='Bicep'
+                    body2='Tricep'
+                    body3='Vareta'
                     />
                 </div>
             </div>    
