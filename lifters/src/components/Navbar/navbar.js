64981d07c40cc578ac21e54d52d14ca011cbbe25
Bernardo Reis <bma.reis@campus.fct.unl.pt>
2022-11-28 20:00:01 +0000

    navbar updated

--- lifters/src/components/Navbar/navbar.js
+++ lifters/src/components/Navbar/navbar.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, {useEffect, useRef, useState} from 'react'
 import { Link } from 'react-router-dom'
 
 import Logo from '../../images/Logo_Branco.png'
@@ -6,15 +6,43 @@ import Logo from '../../images/Logo_Branco.png'
 import { FaBars, FaTimes } from 'react-icons/fa'
 
 import './navbar.css'
+import {CgProfile} from "react-icons/cg";
+import {MdEditCalendar} from "react-icons/md";
+import {GiHealthDecrease} from "react-icons/gi";
+import {FiLogOut} from "react-icons/fi";
+import format from "date-fns/format";
 
 function Navbar() {
   const [click, setClick] = useState(false);
+  const [buttonPopUp, setButtonPopup] = useState(false)
   const handleClick = () => setClick(!click);
 
+  function logOut(){
+      sessionStorage.clear()
+  }
+    useEffect (() => {
+        document.addEventListener("keydown", hideOnEscape, true)
+        document.addEventListener("click", hideOnClick, true)
+    })
+
+    const hideOnEscape = (e) => {
+        if(e.key === "Escape") {
+            setButtonPopup(false)
+        }
+    }
+
+    const refOne = useRef(null)
+
+    const hideOnClick = (e) => {
+        if(refOne.current && !refOne.current.contains(e.target)) {
+            setButtonPopup(false)
+        }
+    }
+
   return (
-    <nav className='navbar'>
+    <div className='navbar'>
       <Link to="/overview" className='navbar-logo'>
-          <img src={Logo} alt="Logo" width={60} height={60}/>  {/* Width = 0.9 * Height */}
+          <img src={Logo} alt="Logo" width={45} height={50}/>  {/* Width = 0.9 * Height */}
           Lifters Journal
       </Link>   
 
@@ -23,21 +51,42 @@ function Navbar() {
                 : <FaBars size={30} style={{color: '#ffffff'}}/>}
       </div>   
         
-      <ul className={click ? 'navbar-menu active' : 'navbar-menu'}>
-        <li className='navbar-menu-item'>
+      <div className={click ? 'navbar-menu active' : 'navbar-menu'}>
+        <div className='navbar-menu-item'>
           <Link to="/overview" className='navbar-menu-item-link'>Home</Link>
-        </li>    
-        <li className='navbar-menu-item'>
+        </div>
+        <div className='navbar-menu-item'>
           <Link to="/find_gyms" className='navbar-menu-item-link'>Find Gyms</Link>
-        </li>
-        <li className='navbar-menu-item'>
+        </div>
+        <div className='navbar-menu-item'>
           <Link to="/generate_workout" className='navbar-menu-item-link'>Generate Workout</Link>
-        </li>
-        <li className='navbar-menu-item'>
+        </div>
+        <div className='navbar-menu-item'>
           <Link to="/customize_workout" className='navbar-menu-item-link'>Costumize Workout</Link>
-        </li>
-      </ul>
-   </nav>
+        </div>
+        <div className='navbar-menu-item' onClick={() => setButtonPopup(true)}>
+            <CgProfile className="profile-icon-navbar"/>
+        </div>
+      </div>
+        <div className="pop-up-navbar">{
+            (buttonPopUp) ? (
+                <div ref={refOne} className="pop-up-navbar-active">
+                    <div className="pop-up-element-navbar">
+                        <MdEditCalendar className="icon-element-bar"/>
+                        <Link to="/register_workout" className='pop-up-navbar-menu-item-link'>Schedule Workouts</Link>
+                    </div>
+                    <div className="pop-up-element-navbar">
+                        <GiHealthDecrease className="icon-element-bar"/>
+                        <Link to="/injuries" className='pop-up-navbar-menu-item-link'>Register Injuries</Link>
+                    </div>
+                    <div className="pop-up-element-navbar" onClick={logOut()}>
+                        <FiLogOut className="icon-element-bar"/>
+                        <Link to="/" className='pop-up-navbar-menu-item-link'>Log Out</Link>
+                    </div>
+                </div>
+            ):""}
+        </div>
+   </div>
   )
 }
 
